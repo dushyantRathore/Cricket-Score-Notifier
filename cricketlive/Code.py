2d186e1c9bb6f89fb43f00f4dd547138bf51d7f3
dushyantRathore <dushyant.bgs@gmail.com>
2017-04-04 00:07:07 +0530

    Updated

--- cricketlive/Code.py
+++ cricketlive/Code.py
@@ -1,44 +1,36 @@
 import notify2
 import requests
 import json
-import pandas as pd
-from prettytable import PrettyTable
-
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
 
 
-url = "https://powerful-tor-13817.herokuapp.com/live"
+def getscore():
 
-headers = {
-    'cache-control': "no-cache",
-    'postman-token': "48cc4d45-5ce2-f04f-cc27-7f5b80874c59"
-    }
+    url = "https://powerful-tor-13817.herokuapp.com/live"
 
-response = requests.request("POST", url, headers=headers)
-score = response.text
-score = json.loads(score)
-l = len(score["Matches"])
+    headers = {
+        'cache-control': "no-cache",
+        'postman-token': "48cc4d45-5ce2-f04f-cc27-7f5b80874c59"
+        }
 
-li = []
+    response = requests.request("POST", url, headers=headers)
+    score = response.text
+    score = json.loads(score)
 
-for i in range(0,l):
-    li.append(score["Matches"][i]["Team A"] + " vs " + score["Matches"][i]["Team B"])
+    l = len(score["Matches"])
 
-# print li
+    result = ''
 
-# Notification Tool
+    for i in range(0,l):
+        result += str(score["Matches"][i]["Team A"]) + " vs " + str(score["Matches"][i]["Team B"] + "\n")
 
-notify2.init("Cricket Notifier")
-n = notify2.Notification("Live Scores", str(li))
-n.show()
+    print result
+
+    # Notification Tool
+
+    notify2.init("Cricket Notifier")
+    n = notify2.Notification("Live Scores", result)
+    n.show()
+
+if __name__ == "__main__":
+    getscore()
 
